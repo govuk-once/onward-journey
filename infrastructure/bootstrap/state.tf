b4740b5b00cae51e8092b0ab1c828bfb187fb66e
Alice Lee <alice.lee1@digital.cabinet-office.gov.uk>
2025-12-08 15:11:27 +0000

    Enable versioning for the terraform state bucket

--- infrastructure/bootstrap/state.tf
+++ infrastructure/bootstrap/state.tf
@@ -1,3 +1,10 @@
 resource "aws_s3_bucket" "remote-state" {
-  bucket = "onward-journey-infrastructure-state"
+  bucket = "onward-journey-infrastructure-state-${terraform.workspace}"
+}
+
+resource "aws_s3_bucket_versioning" "remote-state-versioning" {
+  bucket = aws_s3_bucket.remote-state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
