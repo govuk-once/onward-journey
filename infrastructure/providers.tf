2c1bb631d0afd331ce48b47ee82533089a624149
Alice Lee <alice.lee1@digital.cabinet-office.gov.uk>
2025-12-09 08:56:16 +0000

    Synchronise terraform provider versions with existing terraform

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,10 +1,15 @@
 terraform {
-  required_version = "1.14.1"
+  required_version = "1.13.5"
 
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "6.25.0"
+      version = "6.21.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
     }
   }
 
