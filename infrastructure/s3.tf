53b29147c7f5b975a97eaef38931da33b4a8305a
Sophie Wenban <74198488+swenban@users.noreply.github.com>
2025-12-19 17:16:26 +0000

    JOUR-18 Add mock RAG dataset to git repo and enable auto upload/update of mock data files to S3 (#10)
    * Ignore zsh shell config files
    
    * Add mock rag data source file
    
    * Upload mock_data files to S3 on terraform apply
    
    * Update app readme to reflect changes to rag data source requirements
    
    * Set default kb_path to simplify interactive mode command
    
    ---------
    
    Co-authored-by: Alice Lee <alice.lee1@digital.cabinet-office.gov.uk>

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -23,3 +23,12 @@ resource "aws_s3_bucket_public_access_block" "dataset_storage" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+# Upload mock data files to s3
+resource "aws_s3_object" "mock_data_upload" {
+  for_each = local.mock_data_files # Iterate over every file found by the fileset function in locals.tf
+  bucket   = aws_s3_bucket.dataset_storage.id
+  key      = "mock/${each.value}"
+  source   = "../mock_data/${each.value}"
+  etag     = filemd5("../mock_data/${each.value}") # Terraform uses this to detect if the local file has changed. If it changes, Terraform will update the S3 object on 'apply'.
+}
