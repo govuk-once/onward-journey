e1528d6cf322f811fe88332bc4295cf65121409f
Alice Lee <alice.lee1@digital.cabinet-office.gov.uk>
2026-01-23 11:31:50 +0000

    Reindent everything with 2 spaces instead of tabs

--- frontend/src/lib/components/ConversationMessage.svelte
+++ frontend/src/lib/components/ConversationMessage.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
-	export interface Props {
-		message: string;
-		user?: string;
-		isSelf: boolean;
-	}
+  export interface Props {
+    message: string;
+    user?: string;
+    isSelf: boolean;
+  }
 
-	let { message, user, isSelf = true }: Props = $props();
+  let { message, user, isSelf = true }: Props = $props();
 </script>
 
 <li class="app-c-conversation-message">
-	<div class="app-c-conversation-message__message {isSelf ? 'app-c-conversation-message__message--user-message' : 'app-c-conversation-message__message--govuk-message'}">
-		<div class="app-c-conversation-message__body {isSelf ? 'app-c-conversation-message__body--user-message' : 'app-c-conversation-message__body--govuk-message'}">
-			{#if user}
-			<div class="app-c-conversation-message__identifier {isSelf ? 'govuk-visually-hidden' : ''}">
-				<strong>{user}</strong>
-			</div>
-			{/if}
-			<div class="govuk-body-m govuk-!-margin-bottom-0">{message}</div>
-		</div>
-	</div>
+  <div class="app-c-conversation-message__message {isSelf ? 'app-c-conversation-message__message--user-message' : 'app-c-conversation-message__message--govuk-message'}">
+    <div class="app-c-conversation-message__body {isSelf ? 'app-c-conversation-message__body--user-message' : 'app-c-conversation-message__body--govuk-message'}">
+      {#if user}
+      <div class="app-c-conversation-message__identifier {isSelf ? 'govuk-visually-hidden' : ''}">
+        <strong>{user}</strong>
+      </div>
+      {/if}
+      <div class="govuk-body-m govuk-!-margin-bottom-0">{message}</div>
+    </div>
+  </div>
 </li>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,26 +3,26 @@
   import ConversationMessageContainer from "$lib/components/ConversationMessageContainer.svelte";
   import QuestionForm from "$lib/components/QuestionForm.svelte";
 
-	let messages: ConversationMessageProps[] = $state([
-		{
-			message: "Hello",
-			user: "GOV.UK Onward Journey Agent",
-			isSelf: false
-		},
-		{
-			message: "I would like to talk to someone",
-			user: "You",
-			isSelf: true
-		}
-	]);
+  let messages: ConversationMessageProps[] = $state([
+    {
+      message: "Hello",
+      user: "GOV.UK Onward Journey Agent",
+      isSelf: false
+    },
+    {
+      message: "I would like to talk to someone",
+      user: "You",
+      isSelf: true
+    }
+  ]);
 </script>
 
 <main class="app-conversation-layout__main" id="main-content">
-	<div class="app-conversation-layout__wrapper app-conversation-layout__width-restrictor">
-		<ConversationMessageContainer messages={messages}/>
+  <div class="app-conversation-layout__wrapper app-conversation-layout__width-restrictor">
+    <ConversationMessageContainer messages={messages}/>
 
-		<!-- For now, this simply adds adds a message to the page when submitted
-		 		 TODO: connect to genesys web messaging guest API and send the message there -->
-		<QuestionForm bind:messages={messages}/>
-	</div>
+    <!-- For now, this simply adds adds a message to the page when submitted
+          TODO: connect to genesys web messaging guest API and send the message there -->
+    <QuestionForm bind:messages={messages}/>
+  </div>
 </main>
