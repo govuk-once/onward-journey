2c7f4b2a62159319ed6573c710526105d178a983
Alice Lee <monotypical@users.noreply.github.com>
2026-01-23 13:51:14 +0000

    Initial chat frontend (#12)
    * Initial svelte template
    
    Created with `npx sv create onward-journey-frontend`
    
    * Split up gitignores
    
    * Use new GOV.UK branding
    
    * Add GOV.UK header and experimental phase banner
    
    * Add readme line about running `mise install`
    
    * Switch to using bare govuk-frontend instead of MHCLG component library
    
    * WIP add conversation message component
    
    * Add text input box
    
    * Add eslint, fix links to use svelte's `resolve()`
    
    * Put product name in the GOV.UK header instead of a separate service navigation
    
    * Add separate components for ConversationMessageContainer and QuestionForm
    
    * Add pre-commit hook for eslint and sveltekit check on frontend files
    
    * Only run terraform pre-commit hooks when changing the terraform files
    
    * Simplify govuk-frontend sass imports
    
    * Add message to conversation history when the question form is submitted
    
    * Update frontend README
    
    * Fix indentation
    
    * Move sass to dev dependencies
    
    * Reindent everything with 2 spaces instead of tabs
    
    * Use UUIDs for message list keys
    
    This prevents key collisions when one side of the conversation sends the same message twice in one session
    
    * Add github action to run svelte-check and eslint on frontend PRs
    
    * Add github actions workflow for infrastructure checks on PRs

---
+++ frontend/src/lib/components/ConversationMessageContainer.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import type { ListableConversationMessageProps } from "$lib/types/ConversationMessage";
+  import ConversationMessage from "$lib/components/ConversationMessage.svelte";
+
+  interface Props {
+    messages: ListableConversationMessageProps[]
+  }
+
+  let { messages }: Props = $props();
+</script>
+
+<div class="app-conversation-layout__message-container">
+  <ul id="app-conversation-layout__list">
+    {#each messages as message (message.id)}
+      <ConversationMessage {...message} />
+    {/each}
+  </ul>
+</div>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import type { ListableConversationMessageProps } from "$lib/types/ConversationMessage";
+  import ConversationMessageContainer from "$lib/components/ConversationMessageContainer.svelte";
+  import QuestionForm from "$lib/components/QuestionForm.svelte";
+  import { v7 as uuid } from "uuid";
+
+  let messages: ListableConversationMessageProps[] = $state([
+    {
+      message: "Hello",
+      user: "GOV.UK Onward Journey Agent",
+      isSelf: false,
+      id: uuid()
+    },
+    {
+      message: "I would like to talk to someone",
+      user: "You",
+      isSelf: true,
+      id: uuid()
+    }
+  ]);
+</script>
+
+<main class="app-conversation-layout__main" id="main-content">
+  <div class="app-conversation-layout__wrapper app-conversation-layout__width-restrictor">
+    <ConversationMessageContainer messages={messages}/>
+
+    <!-- For now, this simply adds adds a message to the page when submitted
+          TODO: connect to genesys web messaging guest API and send the message there -->
+    <QuestionForm bind:messages={messages}/>
+  </div>
+</main>
