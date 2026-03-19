35c2b14b8cd3b95ddb37376f9e54839aea5b0354
alexhiles <alex.hiles@hotmail.com>
2026-03-19 16:11:55 +0000

    bubble and connectionbubble components, as well as the footer (home,chat,settings) and the question form for the mobile added

---
+++ frontend/src/lib/components/Footer.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import homeIcon from "$lib/assets/FooterBar/home.svg";
+    import chatIcon from "$lib/assets/FooterBar/Chat Icon.svg";
+    import settingsIcon from "$lib/assets/FooterBar/settings.svg";
+
+    console.log("home icon", homeIcon);
+</script>
+
+<div class="homebar-container">
+    <div class="homebar-item">
+        <img src={homeIcon} alt="Home icon" class="homebar-image" height="30px" width="30px" />
+        <p>Home</p>
+    </div>
+    <div class="homebar-item">
+        <img src={chatIcon} alt="Chat icon" class="homebar-image" height="30px" width="30px" />
+        <p>Chat</p>
+    </div>
+    <div class="homebar-item">
+        <img src={settingsIcon} alt="Settings icon" class="homebar-image" height="30px" width="30px" />
+        <p>Settings</p>
+    </div>
+</div>
+
+<style>
+    .homebar-container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .homebar-item {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 33%;
+        height: 85px;
+        font-size: 14px;
+    }
+</style>
